2b9426f6c5ada42951d45a7ea730f8001ed808ca
Ebby <ebi.badavi@yahoo.com>
2019-09-24 22:27:29 -0400

    Basics are DONE

--- Exercises/Exercise2/js/script.js
+++ Exercises/Exercise2/js/script.js
@@ -1,3 +1,26 @@
 window.onload = function() {
   console.log("testing");
+
+  // make each box “clickable” and say which box was clicked
+  let handleClick = function(event) {
+
+    let displayText = document.getElementById('displayText');
+    displayText.textContent = `This is Text For Box ${this.id}`;
+    displayText.style.display = "inline-block";
+
+    // hide the text when it's been clicked
+    displayText.addEventListener('click', function() {
+      displayText.style.display = "none";
+    });
+  }
+
+  let cBox = document.getElementById('C');
+  cBox.addEventListener('click', handleClick);
+
+  let dBox = document.getElementById('D');
+  dBox.addEventListener('click', handleClick);
+
+  let eBox = document.getElementById('E');
+  eBox.addEventListener('click', handleClick);
+
 } //on load
