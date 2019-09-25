65fe2ebb4c7d5e6e31fe90915666ee0239d3588c
Ebby <ebi.badavi@yahoo.com>
2019-09-24 22:36:30 -0400

    BONUS is DONE
    added mouseover  & mouseout  functions and event listeners

--- Exercises/Exercise2/js/script.js
+++ Exercises/Exercise2/js/script.js
@@ -2,7 +2,7 @@ window.onload = function() {
   console.log("testing");
 
   // make each box “clickable” and say which box was clicked
-  let handleClick = function(event) {
+  function handleClick(event) {
 
     let displayText = document.getElementById('displayText');
     displayText.textContent = `This is Text For Box ${this.id}`;
@@ -12,15 +12,29 @@ window.onload = function() {
     displayText.addEventListener('click', function() {
       displayText.style.display = "none";
     });
-  }
+  };
+
+  function handleMouseOver(event) {
+    this.style.opacity = "0.5";
+  };
+
+  function handleMouseOut(event) {
+    this.style.opacity = "1.0";
+  };
 
   let cBox = document.getElementById('C');
   cBox.addEventListener('click', handleClick);
+  cBox.addEventListener('mouseover', handleMouseOver);
+  cBox.addEventListener('mouseout', handleMouseOut);
 
   let dBox = document.getElementById('D');
   dBox.addEventListener('click', handleClick);
+  dBox.addEventListener('mouseover', handleMouseOver);
+  dBox.addEventListener('mouseout', handleMouseOut);
 
   let eBox = document.getElementById('E');
   eBox.addEventListener('click', handleClick);
+  eBox.addEventListener('mouseover', handleMouseOver);
+  eBox.addEventListener('mouseout', handleMouseOut);
 
 } //on load
