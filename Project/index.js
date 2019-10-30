38de4eadcb2be2d52c3596ce01adf2fc7ed32197
Ebby <ebi.badavi@yahoo.com>
2019-10-30 03:34:59 -0400

    Sending and receiving Pixel coordinates DONE

--- Project/index.js
+++ Project/index.js
@@ -1,40 +1,60 @@
 let express = require('express');
 let static = require('node-static');
+const portNumber = 4200;
+const app = express();
+let httpServer = require('http').createServer(app);
 
 let clientIdIncrementing = 0;
 let clientIds = [];
 
-const portNumber = 4200;
-const app = express();
 
-
-let httpServer = require('http').createServer(app);
-
-httpServer.listen(portNumber, function () {
+httpServer.listen(portNumber, function() {
   console.log(`server is running on port ${portNumber}.`);
 });
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+  console.log(req.url);
+});
 
 let io = require('socket.io')(httpServer);
-// serever side
-// ___________________________________________________ HandShake
-io.on('connection', function (socket) {
-  // console.log("a user connected");
-  socket.on('join', function (data) {
-    clientIdIncrementing ++;
-    socket.emit('joinedClientId', clientIdIncrementing);
-    console.log('a new user with id ' + clientIdIncrementing + " has entered");
-    // keep track of IDs
-    clientIds.push({id: clientIdIncrementing, socketId: socket.id});
-  });
-});
+
 // client side
 app.use(express.static(__dirname + '/node_modules'));
 
+// serever side
+// ___________________________________________________ HandShake
+io.on('connection', function(socket) {
+  // console.log("a user connected");
+  socket.on('join', function(data) {
+    clientIdIncrementing++;
+    socket.emit('joinedClientId', clientIdIncrementing);
+    console.log('a new user with id ' + clientIdIncrementing + " has entered");
+    // keep track of IDs
+    clientIds.push({
+      id: clientIdIncrementing,
+      socketId: socket.id
+    });
 
-app.use(express.static(__dirname + '/public'));
+    socket.on('receiveMove', function (data) {
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-  console.log(req.url);
+        // This line sends the event (broadcasts it)
+        // to everyone except the originating client.
+        socket.broadcast.emit('movingFromServer', data);
+       //for testing do one
+       //  socket.emit('movingFromServer', data);
+    });
+
+    socket.on('receiveClick', function (data) {
+
+        // This line sends the event (broadcasts it)
+        // to everyone except the originating client.
+        socket.broadcast.emit('clickFromServer', data);
+       //for testing do one
+       //  socket.emit('movingFromServer', data);
+    });
+
+  });
 });
--- Project/public/js/script.js
+++ Project/public/js/script.js
@@ -1,12 +1,98 @@
-let clientSocket = io.connect('http://localhost:4200');
+window.onload = function() {
+  console.log("launched client script");
+  let clientSocket = io.connect('http://localhost:4200');
 
-//___________________________________________________ HANDSHAKE
-let socketId = -1;
-clientSocket.on('connect', function (data) {
-  console.log("connected");
-  clientSocket.emit('join', 'msg:: client joined');
-  clientSocket.on('joinedClientId', function (data) {
-    socketId = data;
-    console.log(`my ID: ${socketId}`);
+  //___________________________________________________ HANDSHAKE
+  let socketId = -1;
+
+  clientSocket.on('connect', function(data) {
+    console.log("connected");
+    clientSocket.emit('join', 'msg:: client joined');
+    clientSocket.on('joinedClientId', function(data) {
+      socketId = data;
+      console.log(`my ID: ${socketId}`);
+    });
+
+
+
+
+    /* reveive message from server that mouse moved (mousemove) */
+    clientSocket.on('movingFromServer', function(data) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(img,Number(data.x),Number(data.y), 15,15);
+      //console.log("moving");
+
+    });
+
+    /* reveive message from server that mouse stopped moving (UP) */
+    // client.on('receiveStopFromServer', function(data) {
+    //   console.log("receivedStop");
+    //   tool.started = false;
+    // });
+    /* reveive message from server that mouse started moving (DOWN) */
+    clientSocket.on('clickFromServer', function(data) {
+      console.log("receivedClick");
+      context.drawImage(img,Number(data.x),Number(data.y), 15,15);
+
+    });
+
+
+
+
+    //___________________________________________________ CANVAS
+    let canvas = document.getElementById("canvas");
+    context = canvas.getContext('2d');
+    const img = new Image();
+    img.src = '/images/icon.png';
+
+
+
+    //canvas.addEventListener('mousedown', eventOnCanvas, false);
+    canvas.addEventListener('mousemove', eventOnCanvas, false);
+    canvas.addEventListener('click', eventOnCanvas, false);
+
+    function handleClick(ev) {
+      //context.drawImage(img,ev._x,ev._y, 15,15);
+
+      clientSocket.emit('receiveClick', {
+        'x': ev._x,
+        'y': ev._y
+      });
+
+    };
+
+    function handleMouseMove(ev) {
+      clientSocket.emit('receiveMove', {
+        'x': ev._x,
+        'y': ev._y
+      });
+    };
+
+
+    function eventOnCanvas(ev) {
+      // Firefox and chrome
+      if (ev.layerX || ev.layerX == 0) {
+        // get the absolute pos of the element (canvas)
+        var domRect = document.getElementById("canvas").getBoundingClientRect();
+        // note drawing coordinates start at 0,0 in the canvas - so we need the diff
+        // between the mouse and where the canvas starts  ...
+        var diffX = ev.clientX - domRect.x;
+        var diffY = ev.clientY - domRect.y;
+        ev._x = diffX;
+        ev._y = diffY;
+      }
+      /*
+         Call the event handler of the within the pencilTool class
+         could be mousedown, mouseup, mousemove...
+      */
+      if (ev.type === "click") {
+        handleClick(ev);
+      }
+
+      if (ev.type === "mousemove") {
+        handleMouseMove(ev);
+      }
+    }
+
   });
-});
+};
